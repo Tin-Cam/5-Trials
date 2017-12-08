9584c3bbbf2ad3d1a87ad299f082f8b5304cf1fc
N0rmack <mack679@hotmail.com.au>
2017-12-08 22:29:20 +1100

    Began working on code for a health bar

---
+++ 5_Trials_2018/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public float speed;
+    public float maxLength;
+    public float minLength;
+
+    float length;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        length = length + speed;
+
+        if (length >= maxLength || length <= minLength)
+            speed = -speed;
+
+        transform.localScale = new Vector3(length, transform.localScale.y, transform.localScale.z);
+	}
+}
