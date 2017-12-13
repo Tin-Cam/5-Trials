8a2a4b3e13f3add1887c85d7f8cfe2e13d2eaa9d
Cameron <mack679@hotmail.com.au>
2017-12-13 20:46:43 +1100

    Started developing the sword attack while also played around with rotating objects

--- 5_Trials_2018/Assets/Scripts/HealthBar.cs
+++ 5_Trials_2018/Assets/Scripts/HealthBar.cs
@@ -7,6 +7,7 @@ public class HealthBar : MonoBehaviour {
     public float speed;
     public float maxLength;
     public float minLength;
+    public Vector3 point = new Vector3(0, 0, 0);
 
     float length;
 	// Use this for initialization
@@ -21,6 +22,12 @@ public class HealthBar : MonoBehaviour {
         if (length >= maxLength || length <= minLength)
             speed = -speed;
 
-        transform.localScale = new Vector3(length, transform.localScale.y, transform.localScale.z);
+        //transform.localScale = new Vector3(length, transform.localScale.y, transform.localScale.z);
+
+        //Vector3 point = new Vector3(0, 0, 0);
+        Vector3 axis = new Vector3(0, 0, 1);
+        
+
+        transform.RotateAround(point, axis, 1);
 	}
 }
---
+++ 5_Trials_2018/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private PlayerMove playerMove;
+
+    public bool canAttack = true;
+    public float attackSpeed;
+    public float attackRefresh;
+    public float attackHoldTime;
+    public float attackDistance;
+    public GameObject sword;
+
+    void Start()
+    {
+        playerMove = GetComponent<PlayerMove>();
+        sword.SetActive(false);
+    }
+
+    void Update()
+    {
+        Vector3 point = transform.position;
+        Vector3 axis = new Vector3(0, 0, 1);
+
+
+        sword.transform.RotateAround(point, axis, attackSpeed);
+
+        if (!canAttack)
+            return;
+
+        Attack();
+
+    }
+
+    //Stops the player from moving, then starts the Attack Coroutine
+    private void Attack()
+    {
+        if (Input.GetButtonDown("Attack"))
+        {
+            canAttack = false;
+            playerMove.canMove = false;
+            StartCoroutine("AttackCo");
+        }
+    }
+
+    private IEnumerator AttackCo()
+    {
+        sword.transform.position = new Vector2(transform.position.x, 1.5f);
+        sword.SetActive(true);
+
+        canAttack = true;
+        playerMove.canMove = true;
+        yield return new WaitForSeconds(1);
+    }
+}
