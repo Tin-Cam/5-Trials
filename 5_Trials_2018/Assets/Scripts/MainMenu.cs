c53c89d75f61b8abe0bba685438a13a14f03b1a1
Cameron <mack679@hotmail.com.au>
2017-12-19 14:39:40 +1100

    Title screen at a usable state. Will return later to make it pretty <3

---
+++ 5_Trials_2018/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startingScene;
+
+    public void startGame(int difficulty)
+    {
+        //TODO: Make a diffculty setting
+
+        SceneManager.LoadScene(startingScene);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
