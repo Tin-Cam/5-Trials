2aa1ff0221e114a17a912f1d818ab9c582e58033
Cameron <mack679@hotmail.com.au>
2018-03-22 17:30:17 +1100

    Created basic scripts for enemies

---
+++ 5_Trials_2018/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float maxHealth;
+    private float health;
+
+    //Reduces the enemy's health
+	public void takeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            killEnemy();
+        }
+    }
+
+    //Controls removing the enemy from the scene
+    private void killEnemy()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ 5_Trials_2018/Assets/Scripts/Enemy_Speyeder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Speyeder : Enemy {
+
+    public float attackSpeed;
+    public float moveSpeed;
+
+    public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void moveRandom()
+    {
+         
+    }
+}
